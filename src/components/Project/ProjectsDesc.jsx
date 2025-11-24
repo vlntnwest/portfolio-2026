06e734bede146f92074c93bd3c3ef7b653f02f27
Valentin WESTERMEYER <valentin@westermeyer.fr>
2025-11-24 08:53:31 +0100

    project desc

--- src/components/Project/ProjectsDesc.jsx
+++ src/components/Project/ProjectsDesc.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-
 const ProjectsDesc = ({ project }) => {
   return (
     <div className="text-center mt-4">
-      <h2 className="text-lg/6 md:text-2xl/8 font-medium">{project?.label}</h2>
-      <p className="text-sm/6 md:text-base/6 text-gray-600">{project?.tag}</p>
+      <h2 className="text-lg/6 md:text-2xl/8 font-medium">
+        {project?.label || "Project Title"}
+      </h2>
+      <p className="text-sm/6 md:text-base/6 text-gray-600">
+        {project?.tag || "Project Tag"}
+      </p>
     </div>
   );
 };
