cd40c6308c0cd3fbeb3d722e71605c7e0e227536
Valentin WESTERMEYER <valentin@westermeyer.fr>
2025-12-02 18:20:26 +0100

    active project

--- src/app/playground/page.jsx
+++ src/app/playground/page.jsx
@@ -1,20 +1,35 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import projectPlayground from "@/lib/playground.json";
 import { useInfiniteScroll } from "@/hooks/useInfiniteClone";
+import { useWheelContext } from "@/contexts/WheelContext";
 
 const Page = () => {
   const [showModal, setShowModal] = useState(false);
-  const [src, setSrc] = useState(null);
+  const [src, setSrc] = useState(1);
   const items = useInfiniteScroll(projectPlayground, 400);
 
   const baseBlobUrl = process.env.NEXT_PUBLIC_BASE_BLOB_URL;
+
+  const [isActive, setIsActive] = useState(-1);
+  const { dir } = useWheelContext();
 
   const handleClick = (src) => {
     setShowModal((s) => !s);
     setSrc(src);
   };
+
+  useEffect(() => {
+    if (items.length < 0) return;
+
+    if (dir === 0) return;
+    if (dir >= 1) {
+      setIsActive((prev) => prev + 1);
+    } else if (dir <= -1) {
+      setIsActive((prev) => prev - 1);
+    }
+  }, [dir]);
 
   return (
     <div className="overflow-y-scroll">
@@ -44,14 +59,18 @@ const Page = () => {
             onClick={() => handleClick(`${baseBlobUrl}/${item.src}`)}
           >
             <div className="aspect-square">
-              <Image
-                src={`${baseBlobUrl}/${item.src}`}
-                alt={item.label}
-                width={item.width}
-                height={item.height}
-                className="w-full h-full object-contain group-hover:scale-90 transition-all duration-300 ease-in-out"
-                loading="eager"
-              />
+              <div
+                className={`p-2 rounded-xs ${isActive === index && "bg-black"}`}
+              >
+                <Image
+                  src={`${baseBlobUrl}/${item.src}`}
+                  alt={item.label}
+                  width={item.width}
+                  height={item.height}
+                  className="w-full h-full rounded-xs object-contain group-hover:scale-90 transition-all duration-300 ease-in-out"
+                  loading="eager"
+                />
+              </div>
             </div>
             <div className="text-center pt-[10px] flex justify-center inner">
               <span>0</span>
