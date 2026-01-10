601af4b4ff6e1b96a54f4548c7713da081ddf129
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2026-01-10 12:18:40 +0100

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
         <main className="flex flex-col h-auto">
           <Header />
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
